0b3486845421a333c89a121b50ddedb68f0af28d
Kasumi Hanazuki <kasumi@rollingapple.net>
2015-06-09 16:27:25 +0900

    Associate the root route with the users index

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-  root 'application#hello'
+  root 'users#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
