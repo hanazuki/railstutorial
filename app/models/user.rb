b0d11e56381fd137460fa02916450399918c21cb
Kasumi Hanazuki <kasumi@rollingapple.net>
2015-06-11 14:59:19 +0900

    Implement User model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ActiveRecord::Base
+  # HTML5-compatible email-format validator
+  VALID_EMAIL_REGEX = /\A[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*\z/
+
+  before_save { self.email = email.downcase }
+
+  validates :name, presence: true, length: {maximum: 50}
+  validates :email, presence: true, length: {maximum: 50},
+            format: {with: VALID_EMAIL_REGEX},
+            uniqueness: {case_sensitive: false}
 end
