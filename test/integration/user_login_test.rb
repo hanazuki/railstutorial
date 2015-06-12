b7f530e917980cd37ffbf4a6572e98176bf8775f
Kasumi Hanazuki <kasumi@rollingapple.net>
2015-06-12 15:45:26 +0900

    Add integration test for logging-in

--- test/integration/user_login_test.rb
+++ test/integration/user_login_test.rb
@@ -2,6 +2,10 @@ require 'test_helper'
 
 class UserLoginTest < ActionDispatch::IntegrationTest
 
+  def setup
+    @user = users(:michael)
+  end
+
   test 'login with invalid information' do
     get login_path
     assert_template 'sessions/new'
@@ -12,5 +16,18 @@ class UserLoginTest < ActionDispatch::IntegrationTest
 
     get root_path
     assert flash.empty?
+  end
+
+  test 'login with valid information' do
+    get login_path
+    assert_select 'a[href=?]', login_path
+    assert_select 'a[href=?]', logout_path, count: 0
+    post login_path, session: {email: @user.email, password: 'password'}
+    assert_redirected_to @user
+    follow_redirect!
+    assert_template 'users/show'
+    assert_select 'a[href=?]', login_path, count: 0
+    assert_select 'a[href=?]', logout_path
+    assert_select 'a[href=?]', user_path(@user)
   end
 end
