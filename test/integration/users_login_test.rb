098ae522ba4b305aeccdbe6e11eeecfcb17a55b9
Kasumi Hanazuki <kasumi@rollingapple.net>
2015-06-12 16:53:47 +0900

    Bugfix: A user clicking logout in multiple windows causes an exception

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -35,6 +35,7 @@ class UserLoginTest < ActionDispatch::IntegrationTest
     delete logout_path
     assert_not is_logged_in?
     assert_redirected_to root_url
+    delete logout_path # simulate a user clicking logout in a second window
     follow_redirect!
     assert_select 'a[href=?]', login_path
     assert_select 'a[href=?]', logout_path, count: 0
