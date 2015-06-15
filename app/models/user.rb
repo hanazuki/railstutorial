21ad29e4bbbf977aa620b5f95b639696af625c34
Kasumi Hanazuki <kasumi@rollingapple.net>
2015-06-15 14:58:40 +0900

    Implement editable user profile

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,7 @@ class User < ActiveRecord::Base
             uniqueness: {case_sensitive: false}
 
   has_secure_password
-  validates :password, presence: true, length: {minimum: 6}
+  validates :password, presence: true, length: {minimum: 6}, allow_nil: true
 
   # Remembers a user in the database for use in persistent sessions.
   def remember
