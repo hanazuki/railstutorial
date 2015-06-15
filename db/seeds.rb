ab53dc1782eef3786b7c8a2c997adc06d2db79da
Kasumi Hanazuki <kasumi@rollingapple.net>
2015-06-15 18:48:28 +0900

    Add User.admin field

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,6 @@
 User.create!(name: 'Example User', email: 'example@railstutorial.org',
-             password: 'foobar', password_confirmation: 'foobar')
+             password: 'foobar', password_confirmation: 'foobar',
+             admin: true)
 
 99.times do |n|
   name  = Faker::Name.name
@@ -8,4 +9,3 @@ User.create!(name: 'Example User', email: 'example@railstutorial.org',
   User.create!(name: name, email: email,
                password: password, password_confirmation: password)
 end
-
