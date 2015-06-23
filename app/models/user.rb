533def5ebb638d89ec067b1ecab1866cf223754f
Kasumi Hanazuki <kasumi@rollingapple.net>
2015-06-23 16:28:08 +0900

    Add Micropost model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  has_many :microposts, dependent: :destroy
   attr_accessor :remember_token, :activation_token, :reset_token
 
   # HTML5-compatible email-format validator
