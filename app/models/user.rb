1bb2bb08e91773f5c9882365068dbc70dad7aa4e
Kasumi Hanazuki <kasumi@rollingapple.net>
2015-07-02 13:52:33 +0900

    Solve N+1 query problem

--- app/models/user.rb
+++ app/models/user.rb
@@ -76,7 +76,7 @@ class User < ActiveRecord::Base
 
   def feed
     following_ids = 'SELECT followed_id FROM relationships WHERE  follower_id = :user_id'
-    Micropost.where("user_id IN (#{following_ids}) OR user_id = :user_id", user_id: id)
+    Micropost.where("user_id IN (#{following_ids}) OR user_id = :user_id", user_id: id).includes(:user)
   end
 
   # Follows a user.
