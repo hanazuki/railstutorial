93f128c9ea84e5651a9f3ce32fb74d993496edd9
Kasumi Hanazuki <kasumi@rollingapple.net>
2015-07-10 12:10:26 +0900

    always use local storage

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -4,11 +4,7 @@ class PictureUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
   process resize_to_limit: [400, 400]
 
-  if Rails.env.production?
-    storage :fog
-  else
-    storage :file
-  end
+  storage :file
 
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
