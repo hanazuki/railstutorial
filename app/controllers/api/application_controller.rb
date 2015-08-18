6aff43e08a1f16be718d9da5260cc345f9fa158a
Joe-noh <goflb.jh@gmail.com>
2015-08-18 16:33:30 +0900

    Api名前空間以下にmicroposts controller作成

---
+++ app/controllers/api/application_controller.rb
@@ -0,0 +1,15 @@
+class Api::ApplicationController < ActionController::Base
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  protect_from_forgery with: :null_session
+
+  include SessionsHelper
+
+  # Confirms a logged-in user
+  def logged_in_user
+    unless logged_in?
+      @messages = ["Unauthorized"]
+      render "api/shared/error_messages", status: :unauthorized
+    end
+  end
+end
---
+++ app/controllers/api/microposts_controller.rb
@@ -0,0 +1,30 @@
+class Api::MicropostsController < Api::ApplicationController
+  before_action :logged_in_user, only: [:create, :destroy]
+  before_action :correct_user, only: :destroy
+
+  def create
+    @micropost = current_user.microposts.build(micropost_params)
+    if @micropost.save
+      render json: @micropost, status: :created
+    else
+      @messages = @micropost.errors.full_messages
+      render "api/shared/error_messages.json", status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @micropost.destroy
+    render json: "", status: :accepted
+  end
+
+  private
+
+  def micropost_params
+    params.require(:micropost).permit(:content, :picture)
+  end
+
+  def correct_user
+    @micropost = current_user.microposts.find_by(id: params[:id])
+    redirect_to root_url unless @micropost
+  end
+end
