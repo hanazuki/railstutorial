47713ad62601b047d3088d37751d3d61cd92288c
Joe-noh <goflb.jh@gmail.com>
2015-08-20 14:43:29 +0900

    Api::UsersControllerを実装

---
+++ app/controllers/api/users_controller.rb
@@ -0,0 +1,78 @@
+class Api::UsersController < Api::ApplicationController
+  before_action :logged_in_user, only: [:index, :update, :destroy, :following, :followers]
+  before_action :correct_user, only: [:update]
+  before_action :admin_user, only: [:destroy]
+
+  def index
+    @users = User.where(activated: true).paginate(page: params[:page])
+  end
+
+  def show
+    @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
+    render_errors ["Account not activated"], status: :forbidden unless @user.activated
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      @user.send_activation_email
+      render json: "", stauts: :created
+    else
+      render_errors @user.errors.full_messages, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user && @user.update_attributes(user_params)
+      render json: @user, status: :accepted
+    else
+      render_errors @user.errors.full_messages, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    render json: "", stauts: :accepted
+  end
+
+  def following
+    @title = t('following', scope: 'users.show_follow')
+    @user  = User.find(params[:id])
+    @users = @user.following.paginate(page: params[:page])
+
+    render json: @users
+  end
+
+  def followers
+    @title = t('followers', scope: 'users.show_follow')
+    @user  = User.find(params[:id])
+    @users = @user.followers.paginate(page: params[:page])
+
+    render json: @users
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+  # Before filters
+
+  # Confirms the correct user.
+  def correct_user
+    @user = User.find(params[:id])
+    render_forbidden unless current_user?(@user)
+  end
+
+  # Confirms an admins user.
+  def admin_user
+    render_forbidden unless current_user.admin?
+  end
+
+  def render_forbidden
+    render_errors ["You don't have permission to access"], status: :forbidden
+  end
+end
