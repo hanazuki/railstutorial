c19e16a689c2d9a0d28ee25cefc69032dba041de
Joe-noh <goflb.jh@gmail.com>
2015-08-24 18:30:17 +0900

    sessionが絡むapiのテストを変更

--- test/controllers/api/feed_controller_test.rb
+++ test/controllers/api/feed_controller_test.rb
@@ -4,17 +4,18 @@ class Api::FeedControllerTest < ActionController::TestCase
 
   def setup
     @user = users(:michael)
+    @request.headers["Authorization"] = token_for(@user)
   end
 
   test 'should return micropost feed as json' do
-    log_in_as(@user)
-    get :index
+    get :index, {}
 
     assert_equal 200, response.status
     assert_equal @user.feed.to_json, response.body
   end
 
-  test 'should return 402 if user is not logged in' do
+  test 'should return 402 if auth token is missing' do
+    @request.headers["Authorization"] = ""
     get :index
 
     assert 402, response.status
--- test/controllers/api/microposts_controller_test.rb
+++ test/controllers/api/microposts_controller_test.rb
@@ -5,10 +5,10 @@ class Api::MicropostsControllerTest < ActionController::TestCase
   def setup
     @user = users(:michael)
     @micropost = microposts(:orange)
+    @request.headers["Authorization"] = token_for(@user)
   end
 
   test 'should return created micropost as json' do
-    log_in_as(@user)
     post :create, micropost: {content: "Lorem ipsum"}
 
     json = JSON.parse(response.body)
@@ -22,7 +22,6 @@ class Api::MicropostsControllerTest < ActionController::TestCase
   end
 
   test 'should return errors when params are invalid' do
-    log_in_as(@user)
     post :create, micropost: {content: ""}
 
     json = JSON.parse(response.body)
@@ -32,13 +31,14 @@ class Api::MicropostsControllerTest < ActionController::TestCase
   end
 
   test 'should return 202 when deleted the micropost' do
-    log_in_as(@user)
     delete :destroy, id: @micropost
 
     assert_equal 202, response.status
   end
 
-  test 'should return 401 if user was not logged in' do
+  test 'should return 401 if auth token is missing' do
+    @request.headers["Authorization"] = ""
+
     post :create, micropost: {content: "Lorem inpsum"}
 
     json = JSON.parse(response.body)
@@ -48,8 +48,8 @@ class Api::MicropostsControllerTest < ActionController::TestCase
   end
 
   test 'should return 403 if current_user is not the owner' do
-    other = users(:archer)
-    log_in_as(other)
+    @request.headers["Authorization"] = token_for(users :archer)
+
     delete :destroy, id: @micropost
 
     assert_equal 403, response.status
