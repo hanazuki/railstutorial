c96298048971b4d849f1898ef284b4d594d41d7a
Joe-noh <goflb.jh@gmail.com>
2015-08-25 12:28:02 +0900

    環境変数HOST_NAMEでメールに載るホスト名を指定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = {host: 'agile-fjord-2695.herokuapp.com'}
+  config.action_mailer.default_url_options = {host: ENV['HOST_NAME']}
   ActionMailer::Base.smtp_settings = {
     address: ENV['SMTP_HOST'],
     port: ENV['SMTP_PORT'] || 587,
