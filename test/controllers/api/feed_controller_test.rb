79cbd2bef65752b75875f406a48ebe2f1712b0c5
Joe-noh <goflb.jh@gmail.com>
2015-08-27 15:37:44 +0900

    /api/feedをpaginationするように変更

--- test/controllers/api/feed_controller_test.rb
+++ test/controllers/api/feed_controller_test.rb
@@ -22,6 +22,16 @@ class Api::FeedControllerTest < ActionController::TestCase
     end
   end
 
+  test 'should be able to paginate' do
+    get :index, page: 1, format: :json
+    feed_page_1 = JSON.parse(response.body)
+
+    get :index, page: 2, format: :json
+    feed_page_2 = JSON.parse(response.body)
+
+    assert_not_equal feed_page_1, feed_page_2
+  end
+
   test 'should return 402 if auth token is missing' do
     @request.headers["Authorization"] = ""
     get :index, format: :json
